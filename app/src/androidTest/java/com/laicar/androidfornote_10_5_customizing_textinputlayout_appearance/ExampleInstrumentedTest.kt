6089d6045ac4102fd1e85ed49214322410d1f37b
启辰 <akluse@qq.com>
2019-03-07 21:54:00 +0800

    project init

---
+++ app/src/androidTest/java/com/laicar/androidfornote_10_5_customizing_textinputlayout_appearance/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.laicar.androidfornote_10_5_customizing_textinputlayout_appearance
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.laicar.androidfornote_10_5_customizing_textinputlayout_appearance", appContext.packageName)
+    }
+}
